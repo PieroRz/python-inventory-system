a872a2c203041f57cc2468ac74e46b3600060616
Piero Ruiz <ruizolivarespiero@outlook.com>
2026-07-05 18:01:19 -0500

    Implement Product and Inventory classes

---
+++ src/inventory.py
@@ -0,0 +1,11 @@
+class Inventory:
+    def __init__(self):
+        self.products = []
+
+    def add_product(self, product):
+        self.products.append(product)
+
+    def list_products(self):
+        for product in self.products:
+            product.display_info()
+            print("-" * 20)
--- src/main.py
+++ src/main.py
@@ -5,6 +5,9 @@ if __name__ == "__main__":
     main()
 
 from product import Product
+from inventory import Inventory
+
+inventory = Inventory()
 
 laptop = Product(
     1,
@@ -14,4 +17,16 @@ laptop = Product(
     10
 )
 
-laptop.display_info()
+printer = Product(
+    2,
+    "LaserJet Pro",
+    "Printer",
+    "HP",
+    5
+)
+
+inventory.add_product(laptop)
+inventory.add_product(printer)
+
+inventory.list_products()
+
