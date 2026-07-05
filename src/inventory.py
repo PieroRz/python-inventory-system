16aa96127cecc2820e518db496293755143eeec5
Piero Ruiz <ruizolivarespiero@outlook.com>
2026-07-05 18:38:00 -0500

    Implement inventory CRUD operations

--- src/inventory.py
+++ src/inventory.py
@@ -15,3 +15,17 @@ class Inventory:
             if product.product_id == product_id:
                 return product
         return None
+    
+    def remove_product(self, product_id):
+        product_to_remove = self.find_product_by_id(product_id)
+        if product_to_remove:
+            self.products.remove(product_to_remove)
+            return True
+        return False
+    
+    def update_product_quantity(self, product_id, new_quantity):
+        product = self.find_product_by_id(product_id)
+        if product:
+            product.quantity = new_quantity
+            return True
+        return False
--- src/main.py
+++ src/main.py
@@ -26,13 +26,24 @@ def main():
     inventory.add_product(laptop)
     inventory.add_product(printer)
 
-    product = inventory.find_product_by_id(2)
-
-    if product:
-        product.display_info()
+    if inventory.remove_product(1):
+        print("Product removed.")
     else:
         print("Product not found.")
 
+    if inventory.update_product_quantity(2, 8):
+        print("Product quantity updated.")
+    else:
+        print("Product not found.")
+
+    inventory.list_products()
+
+    # product = inventory.find_product_by_id(1)
+
+    # if product:
+    #     product.display_info()
+    # else:
+    #     print("Product not found.")
 
 if __name__ == "__main__":
     main()
